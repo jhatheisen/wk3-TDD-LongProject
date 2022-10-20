196a0799eb8ae29f7c9241255d0fc7d958e7def6
Jhass Theisen <jhasstheisen1@gmail.com>
2022-10-20 10:59:37 -0700

    passed phase 1

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,7 @@
 module.exports = function reverseString(string) {
-  // Your code here
+  let newString = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+  }
+  return newString;
 };
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,15 @@
-// Your code here
+const { expect } = require('chai');
+
+const reverseString = require('../problems/reverse-string');
+
+describe('Reverse String', function () {
+
+  it('should return the reversed string', function() {
+    expect(reverseString('fun')).to.eql('nuf');
+  });
+
+  it('should throw a type error', function() {
+    expect(() => reverseString(null)).to.throw(TypeError);
+  });
+
+});
