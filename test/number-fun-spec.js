5e4dd0cbde9667d98cbda27023e0cc809a7647d2
Jhass Theisen <jhasstheisen1@gmail.com>
2022-10-20 11:08:40 -0700

    created tests

--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,18 @@
-// Your code here
+const { expect } = require('chai');
+
+const { returnsThree, reciprocal } = require('../problems/number-fun');
+
+describe('returns three', function() {
+
+  it('should return three', function() {
+    expect(returnsThree()).to.eql(3);
+  });
+
+});
+
+describe('should take a num and return the reciprocal', function() {
+
+  it('should return the reciprocal of the number', function() {
+    expect(reciprocal(5)).to.eql(1/5);
+  });
+});
