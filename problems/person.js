8012909a5ad6ce12612c24b0a9b3fb2b42490515
Jhass Theisen <jhasstheisen1@gmail.com>
2022-10-20 16:17:10 -0700

    finished phase 4

--- problems/person.js
+++ problems/person.js
@@ -16,7 +16,42 @@ class Person {
     return otherPerson.visit(this)
   }
 
-  
+  update(obj) {
+    if (typeof obj !== 'object') {
+      throw new TypeError('arguement is not an object');
+    } else if (obj.name === undefined || obj.age === undefined) {
+      throw new TypeError("No Name or Age Properties");
+    } else if (typeof obj === 'object') {
+      this.name = obj.name;
+      this.age = obj.age;
+    }
+  }
+
+  tryUpdate(obj) {
+    try {
+      this.update(obj);
+      if (this.name === obj.name && this.age === obj.age) {
+        return true;
+      }
+      return false;
+
+    }
+    catch (error){
+      return false;
+    }
+  }
+
+  static greetAll(objArr) {
+    let storedGreetings = [];
+    for (let obj of objArr) {
+      storedGreetings.push(obj.sayHello());
+    }
+    return storedGreetings;
+  }
 }
+// let Joe = new Person("Joe",50);
+// let Wendy = new Person("Wendy",40);
+// let people = [Joe, Wendy];
+// console.log(Person.greetAll(people));
 
 module.exports = Person;
--- test/person-spec.js
+++ test/person-spec.js
@@ -5,43 +5,76 @@ const Person = require("../problems/person")
 
 
 describe("Person", function() {
+
+    let Joe;
+    let Wendy;
+
+    beforeEach(() => {
+      Joe = new Person("Joe",50);
+      Wendy = new Person("Wendy",40);
+    });
+
     it ("Should construct name and age", function (){
-        let Joe = new Person("Joe",50)
-        expect(Joe.name).to.exist
-        expect(Joe.age).to.exist
-    })
+        expect(Joe.name).to.exist;
+        expect(Joe.age).to.exist;
+    });
 
     describe("sayHello()",function() {
         it ("Should return the persons name and greeting", function (){
-            let Joe = new Person("Joe",50)
-            expect(Joe.sayHello()).to.eql("Hello there Joe")
-        })
-
-    })
+            expect(Joe.sayHello()).to.eql("Hello there Joe");
+        });
+    });
 
     describe("visit(otherPerson)", function() {
         it ("Should return a string name visited name 2", function(){
-            let Joe = new Person("Joe",50)
-            let Wendy = new Person("Wendy",40)
-            expect(Joe.visit(Wendy)).to.eql("Joe visited Wendy")
-        })
-    })
+            expect(Joe.visit(Wendy)).to.eql("Joe visited Wendy");
+        });
+    });
+
     describe("switchVisit(otherPerson)", function(){
         it ("Should return a string name 2 visited name 1", function(){
-            let Joe = new Person("Joe",50)
-            let Wendy = new Person("Wendy",40)
-            expect(Joe.switchVisit(Wendy)).to.eql("Wendy visited Joe")
-        })
+            expect(Joe.switchVisit(Wendy)).to.eql("Wendy visited Joe");
+        });
+    });
 
-
-    })
     describe("update(obj)", function() {
         it ("Should throw a new TypeError with a message if argument is not an object", function(){
-            let wrongType = 1
-            let Joe = new Person("Joe",50)
-            expect(Joe.update(wrongType)).to.throw(TypeError,"argument is not an object")
-        })
+          let wrongType = 1;
+          expect(() => Joe.update(wrongType)).to.throw(TypeError,"arguement is not an object");
+        });
 
-    })
+        it ('Should update the objects values with the passed in objects values', function(){
+          Joe.update(Wendy);
+          expect(Joe.name).to.eql(Wendy.name);
+          expect(Joe.age).to.eql(Wendy.age);
+        });
 
+        it ('Should return a new TypeError "No Name or Age Properties" if there is no name and age property of the object', function() {
+          let errorPerson = new Person();
+          expect(() => Joe.update(errorPerson)).to.throw(TypeError, "No Name or Age Properties");
+        });
+    });
+
+    describe('tryUpdate(obj)', function() {
+
+      it ('Should call the update(obj) method with the incoming arg and return true', function() {
+        expect(Joe.tryUpdate(Wendy)).to.eql(true);
+        expect(Joe.name).to.eql(Wendy.name);
+        expect(Joe.age).to.eql(Wendy.age);
+      });
+
+      it ('Should return false if improperly invoked', function() {
+        let badObj = new Person;
+        let badObj2 = 1;
+        expect(badObj.tryUpdate(badObj2)).to.eql(false);
+      });
+    });
+
+    describe('greetAll(objArr)', function() {
+
+      it ('Should call sayHello() on each object element of the passed in array ', function() {
+        let people = [Joe, Wendy];
+        expect(Person.greetAll(people)).to.eql([`Hello there Joe`, `Hello there Wendy`]);
+      });
+    });
 })
