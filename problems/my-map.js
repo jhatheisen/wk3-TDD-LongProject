50c15ca1c1cd283c5522afce6a5600aee89ecc82
Jhass Theisen <jhasstheisen1@gmail.com>
2022-10-20 14:22:01 -0700

    phase 3 finished

--- problems/my-map.js
+++ problems/my-map.js
@@ -1,5 +1,22 @@
 function myMap(inputArray, callback) {
-  // Your code here
+
+  // mutates
+  // console.log(inputArray);
+  // for(let i = 0; i < inputArray.length; i++) {
+  //   inputArray[i] = callback(inputArray[i]);
+  // }
+  // console.log(inputArray)
+  // return inputArray;
+
+  let newArr = [];
+
+  for(let el of inputArray) {
+    newArr.push(callback(el));
+  }
+
+  return newArr;
+
+  // return inputArray.map(callback);
 }
 
 module.exports = myMap;
--- test/my-map-spec.js
+++ test/my-map-spec.js
@@ -1,1 +1,42 @@
-// Your code here
+const spies  = require('chai-spies');
+const {expect} = require('chai');
+const chai = require('chai');
+chai.use(spies);
+
+
+const myMap = require('../problems/my-map');
+
+describe('my map', function() {
+
+
+  let arr;
+  let callback;
+
+  beforeEach(() => {
+    arr = [1, 2, 3];
+    callback = (el) => el * 2;
+  });
+
+  it('should return an new array with the values of calling the callback on each element of the array', function () {
+    expect(myMap(arr,callback)).to.eql([2,4,6]);
+  });
+
+  it('should not mutate original arr', function() {
+    let unchanged = arr.slice();
+    myMap(arr,callback);
+    expect(arr).to.eql(unchanged);
+  });
+
+  it('should not call built in array.map', function() {
+    let spy = chai.spy.on(arr, 'map');
+    myMap(arr, callback);
+    expect(spy).to.not.have.been.called();
+  });
+
+  it('should call the callback once for each element in the array', function() {
+    let spy = chai.spy(callback);
+    myMap(arr, spy);
+    expect(spy).to.have.been.called.exactly(arr.length);
+  });
+
+});
