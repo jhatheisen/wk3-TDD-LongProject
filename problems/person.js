1cf78a2e3455f5e6ab8876f3eee71229fdf3ceaf
Oleg Volkov <olegv3@gmail.com>
2022-10-20 18:20:57 -0400

    up to Person

--- problems/person.js
+++ problems/person.js
@@ -1,5 +1,22 @@
 class Person {
-  // Your code here
+  constructor(name,age){
+    this.name = name
+    this.age = age
+  }
+
+  sayHello() {
+    return `Hello there ${this.name}`
+  }
+
+  visit(otherPerson) {
+    return `${this.name} visited ${otherPerson.name}`
+  }
+
+  switchVisit(otherPerson) {
+    return otherPerson.visit(this)
+  }
+
+  
 }
 
 module.exports = Person;
--- test/person-spec.js
+++ test/person-spec.js
@@ -1,1 +1,47 @@
-// Your code here
+const chai = require("chai")
+const expect = chai.expect
+
+const Person = require("../problems/person")
+
+
+describe("Person", function() {
+    it ("Should construct name and age", function (){
+        let Joe = new Person("Joe",50)
+        expect(Joe.name).to.exist
+        expect(Joe.age).to.exist
+    })
+
+    describe("sayHello()",function() {
+        it ("Should return the persons name and greeting", function (){
+            let Joe = new Person("Joe",50)
+            expect(Joe.sayHello()).to.eql("Hello there Joe")
+        })
+
+    })
+
+    describe("visit(otherPerson)", function() {
+        it ("Should return a string name visited name 2", function(){
+            let Joe = new Person("Joe",50)
+            let Wendy = new Person("Wendy",40)
+            expect(Joe.visit(Wendy)).to.eql("Joe visited Wendy")
+        })
+    })
+    describe("switchVisit(otherPerson)", function(){
+        it ("Should return a string name 2 visited name 1", function(){
+            let Joe = new Person("Joe",50)
+            let Wendy = new Person("Wendy",40)
+            expect(Joe.switchVisit(Wendy)).to.eql("Wendy visited Joe")
+        })
+
+
+    })
+    describe("update(obj)", function() {
+        it ("Should throw a new TypeError with a message if argument is not an object", function(){
+            let wrongType = 1
+            let Joe = new Person("Joe",50)
+            expect(Joe.update(wrongType)).to.throw(TypeError,"argument is not an object")
+        })
+
+    })
+
+})
