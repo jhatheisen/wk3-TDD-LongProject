81c611f3478d9b8c23b01e1b74f3d75fca5ad7e4
Jhass Theisen <jhasstheisen1@gmail.com>
2022-10-20 11:18:00 -0700

    finished phase 2

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -1,9 +1,12 @@
 function returnsThree() {
-  // Your code here
+  return 3;
 }
 
 function reciprocal(n) {
-  // Your code here
+  if (n > 1000000 || n < 1) {
+    throw new TypeError('num must be from 1-1000000');
+  }
+  return 1/n;
 }
 
 module.exports = {
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -13,6 +13,11 @@ describe('returns three', function() {
 describe('should take a num and return the reciprocal', function() {
 
   it('should return the reciprocal of the number', function() {
-    expect(reciprocal(5)).to.eql(1/5);
+    expect(reciprocal(5/1)).to.eql(1/5);
   });
+
+  it('should only accept nums from 1-1,000,000', function() {
+    expect(() => reciprocal(1000001)).to.throw(TypeError);
+    expect(() => reciprocal(-10)).to.throw(TypeError);
+  })
 });
