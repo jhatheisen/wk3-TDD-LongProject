e0a5ac37681bcedb26f217d031961fbb26e64c7d
Jhass Theisen <jhasstheisen1@gmail.com>
2022-10-20 17:10:52 -0700

    Working on phase 5 triangle

--- problems/triangle.js
+++ problems/triangle.js
@@ -1,1 +1,31 @@
-// Your code here
+class Triangle {
+  constructor(side1, side2, side3) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+  }
+
+  getPerimeter() {
+    return this.side1 + this.side2 + this.side3;
+  }
+
+  hasValidSideLength() {
+    let a = this.side1;
+    let b = this.side2;
+    let c = this.side3;
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      return false;
+    }
+    return true;
+  }
+
+  validate() {
+    if (this.hasValidSideLength()) {
+      this.isValid = true;
+    } else {
+      this.isValid = false;
+    }
+  }
+}
+
+module.exports = Triangle;
